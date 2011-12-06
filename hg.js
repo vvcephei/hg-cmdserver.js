5d04fe84a4ac41975d903789dd4455cb87fc8af6
vvcephei <vvceph@gmail.com>
2011-12-06 14:17:53 -0600

    golfing. Also, added output buffering.

--- hg.js
+++ hg.js
@@ -1,20 +1,26 @@
-var util  = require('util'),
-    spawn = require('child_process').spawn,
-    hg    = spawn('hg', ['serve', '--cmdserver','pipe']);
+var util  = require('util');
 
+var spawn = require('child_process').spawn,
+    hg    = spawn('hg', ['serve', '--cmdserver','pipe'], {cwd:process.argv[2]});
 
+var result_printer = function(code,out){
+            console.log('get_encoding:\n  |code:<<<'+code+'>>>\n  |out:<<<'+out+'>>>');
+            console.log(out);
+        };
 var script = [
-    get_encoding,
-    function(){run_command("summary")},
+    function(){get_encoding(result_printer)},
+    function(){run_command("summary", result_printer)},
 ]
 
-var buffered_input = new Buffer(0);
-function append_input(input) {
-    var buffer = new Buffer(buffered_input.length+input.length);
-    buffered_input.copy(buffer);
-    input.copy(buffer,buffered_input.length);
-    buffered_input = buffer;
+// could do this with arbitrary #s of args TODO
+function buffer_concat(buffer0, buffer1){
+    var buffer = new Buffer(buffer0.length + buffer1.length);
+    buffer0.copy(buffer);
+    buffer1.copy(buffer,buffer0.length);
+    return buffer;
 }
+
+var buffered_input = new Buffer(0);
 function read_packet() {
     if (buffered_input.length === 0) {
         return false;
@@ -32,37 +38,48 @@ function read_packet() {
         buffered_input = buffered_input.slice(length+data_offset);
     } else {
         // then we didn't get the whole message. just continue buffering.
-        var packet = {
-            incomplete : true,
-        };
+        return false;
     }
     console.log(packet);
     return packet;
 }
 
+var hello = false;
+function parse_hello(packet) {
+    hello = {};
+    var hello_arr = packet.data.toString('utf8').split(/\n/);
+    hello_arr.forEach(function(val,index,array){
+        var result = /\s*([^:\s]*):\s*(.*)/.exec(val);
+        switch(result[1]) {
+            case "capabilities":
+                hello.capabilities = result[2].trim().split(/\s+/);
+                break;
+            case "encoding":
+                hello.encoding     = result[2].trim();
+                break;
+            default:
+        }
+    });
+    console.log(hello);
+}
 
-var hello_read = false;
+
 hg.stdout.on('data', function (input) {
-    console.log('\n------------------------');
+    console.log('\n--------------------');
     console.log('incoming:');
     console.log(input);
-    append_input(input);
+    buffered_input = buffer_concat(buffered_input,input);
 
     var packet;
     while (packet = read_packet()) {
-        if (packet.incomplete) {
-            // we already saved the partial input. just wait for the next stdin event
-            return;
-        }
-        if (! hello_read ) {
-            // TODO: pay attention to capabilities and encoding
-            console.log("hello: "+packet.channel+","+packet.length+","+packet.data+"\n");
-            hello_read = true;
+        if (! hello ) {
+            parse_hello(packet);
         } else {
             // now, we can get down to business
             switch(packet.channel) {
                 case 'o':
                     // output channel
+                    buffered_output = buffer_concat(buffered_output, packet.data);
                     console.log("output: "+packet.data);
                     break;
                 case 'e':
@@ -71,6 +88,7 @@ hg.stdout.on('data', function (input) {
                     break;
                 case 'r':
                     // result channel
+                    command_callback(parse_exit_code(packet.data), buffered_output);
                     console.log("result: "+parse_exit_code(packet.data));
                     break;
                 case 'd':
@@ -108,8 +126,10 @@ hg.on('exit', function (code) {
   }
 });
 
+var buffered_output = new Buffer(0);
+var command_callback;
 var parse_exit_code;
-function run_command(argv) {
+function run_command(argv, callback) {
     hg.stdin.write("runcommand\n");
     var length = new Buffer(4);
     length.writeUInt32BE(argv.length,0);
@@ -117,9 +137,13 @@ function run_command(argv) {
     hg.stdin.write(length);
     hg.stdin.write(argv);
     parse_exit_code = function(numbuf){return numbuf.readUInt32BE(0);};
+    buffered_output = new Buffer(0);
+    command_callback= callback;
 }
-function get_encoding() {
+function get_encoding(callback) {
     hg.stdin.write("getencoding\n");
     parse_exit_code = function(numbuf){return numbuf.toString('utf8',0);};
+    buffered_output = new Buffer(0);
+    command_callback= callback;
 }
 
